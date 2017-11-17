dc67efe681817ee0af0b78c735757c52062d9e1a
Eddie Rada <eddierada@gmail.com>
2017-11-17 14:53:06 -0500

    cometareo borrado de campo profesion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -33,8 +33,9 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
-        Schema::table('users', function (Blueprint $table) {
+        /*Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('profession');
         });
+        */
     }
 }
